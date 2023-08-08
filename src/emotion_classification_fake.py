b0edcf8c7addf399e78599aada540678bee9b508
Koolding <duernokverdensbedste@hotmail.com>
2023-08-08 22:25:48 +0200

    commit

---
+++ src/emotion_classification_fake.py
@@ -0,0 +1,61 @@
+# install packages
+from transformers import pipeline
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+# data
+file = os.path.join(os.getcwd(), "data", "fake_or_real_news.csv")
+data = pd.read_csv(file, index_col=0)
+
+# Subset to fake data
+data = data[data["label"] == "FAKE"]
+
+
+# classify
+classifier = pipeline("text-classification", 
+                      model="j-hartmann/emotion-english-distilroberta-base",
+                      return_all_scores=True)
+
+
+# run the model with labels of emotion with max score
+max_score_df = []
+
+# loop
+for i in data[["title"]]: 
+    for j in data[i]:
+        emotion = classifier(j)
+        
+        # max score 
+        for emotion_dicts in emotion:
+
+            emotion_list = emotion_dicts #list
+
+            max_score = max(emotion_list, key=lambda x:x['score'])
+            max_score_df.append(max_score)
+
+
+# convert max_score_df to a pandas dataframe
+max_score_df = pd.DataFrame(max_score_df)
+
+# occurrences for each emotion
+plot_df =  max_score_df['label'].value_counts()
+plot_df = pd.DataFrame(plot_df)
+plot_df = plot_df.reset_index()
+
+
+# plot
+# predefined colour list
+colors = {"anger": "red", "fear": "black", "joy": "yellow", "disgust": "green", 
+         "neutral": "grey", "surprise": "pink", "sadness": "blue"}
+
+# bar plot of distribution of emotions
+plt.bar(plot_df["label"], plot_df["count"], color=plot_df['label'].map(colors),
+        width = 0.4)
+plt.xlabel("Emotion labels")
+plt.ylabel("Count")
+plt.title("Emotion classification for every fake news headline", fontsize=10)
+plt.suptitle("Distribution of emotions across fake news data", fontsize=14)
+plt.show()
+plt.savefig('out/emotion_distribution_fake.png')
+
